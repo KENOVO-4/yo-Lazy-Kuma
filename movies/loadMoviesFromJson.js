abec99a3dc206cadf4560e10e1070cfeb64e84bc
KENOVO-4 <jojo20111111111@gmail.com>
2025-04-26 02:58:35 -0700

    Update loadMoviesFromJson.js

--- movies/loadMoviesFromJson.js
+++ movies/loadMoviesFromJson.js
@@ -1,16 +1,17 @@
+// js/loadFromJson.js
 
-fetch('../js/data/movies.json')
+fetch('js/data/anime.json')
   .then(res => res.json())
   .then(data => {
-    const container = document.getElementById('movieList');
+    const container = document.getElementById('animeList');
     container.innerHTML = '';
-    for (const [key, movie] of Object.entries(data)) {
+    for (const [key, anime] of Object.entries(data)) {
       const card = document.createElement('a');
-      card.href = movie.link;
+      card.href = anime.link;
       card.className = "bg-gray-800 hover:bg-gray-700 rounded-lg shadow p-4 block transition";
       card.innerHTML = `
-        <img src="${movie.cover}" alt="${movie.title}" class="w-full h-40 object-cover rounded mb-2">
-        <h2 class="text-xl font-bold text-center">${movie.title}</h2>
+        <img src="${anime.cover}" alt="${anime.title}" class="w-full h-40 object-cover rounded mb-2">
+        <h2 class="text-xl font-bold text-center">${anime.title}</h2>
       `;
       container.appendChild(card);
     }
